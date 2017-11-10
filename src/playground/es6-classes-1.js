4efde698e17afc5bf198879a59658e2b841a6fac
Łukasz Grząśko <lukasz.grzasko@hotmail.com>
2017-11-10 15:18:29 +0100

    Lesson 4.2

--- src/playground/es6-classes-1.js
+++ src/playground/es6-classes-1.js
@@ -1,4 +1,3 @@
-
 class Person {
     constructor(name = 'Anonymous', age = 0) {
         this.name = name;
@@ -12,8 +11,45 @@ class Person {
     }
 }
 
-const me = new Person('Lukasz Grzasko', 25);
-console.log(me.getDescription());
+class Student extends Person {
+    constructor(name, age, major) {
+        super(name, age);
+        this.major = major;
+    }
+    hasMajor() {
+        return !!this.major;
+    }
+    getDescription() {
+        let description = super.getDescription();
 
-const other = new Person();
-console.log(other.getDescription());
+        if (this.hasMajor()) {
+            description += ` Their major is ${this.major}.`
+        }
+
+        return description;
+    }
+}
+
+class Traveler extends Person {
+    constructor(name, age, homeLocation) {
+        super(name, age);
+        this.homeLocation = homeLocation;
+    }
+
+    getGreeting() {
+        let greeting = super.getGreeting();
+
+        if (this.homeLocation) {
+            greeting += ` I'm visiting from ${this.homeLocation}.`
+        }
+
+        return greeting
+    }
+}
+
+
+const me = new Traveler('Lukasz Grzasko', 25, 'Zamosc');
+console.log(me.getGreeting());
+
+const other = new Traveler();
+console.log(other.getGreeting());
